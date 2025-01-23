5d52b3fa4e361596be9f541bb3f0cc251963a480
Mochfahmi17 <mochammadfahmiks@gmail.com>
2025-01-23 15:16:12 +0700

    change style

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 function NavbarComponent() {
   const [active, setActive] = useState(false);
   const [changeColor, setChangeColor] = useState(false);
+  const location = useLocation();
+  console.log(location);
 
   const changeBackgroundColor = () => {
     if (window.scrollY > 10) {
@@ -24,21 +27,21 @@ function NavbarComponent() {
   }
   return (
     <>
-      <nav className={changeColor === true ? "navbar active" : "navbar"}>
-        <a href="/" className="navbar-logo">
+      <nav className={`${changeColor ? "navbar active" : "navbar"} ${location.pathname == "/" ? "text-white" : ""}`}>
+        <a href="/" className={`navbar-logo ${location.pathname === "/" && "text-white"}`}>
           SENAYA CREATIVE
         </a>
-        <div className={active ? "navbar-nav active" : "navbar-nav"}>
-          <NavLink to="/" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
+        <div className={`${active ? "navbar-nav active" : "navbar-nav"}`}>
+          <NavLink to="/" className={`${({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")} ${location.pathname === "/" && "text-white"}`}>
             Home
           </NavLink>
-          <NavLink to="/about" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
+          <NavLink to="/about" className={`${({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")} ${location.pathname === "/" && "text-white"}`}>
             About
           </NavLink>
-          <NavLink to="/portfolio" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
+          <NavLink to="/portfolio" className={`${({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")} ${location.pathname === "/" && "text-white"}`}>
             Portfolio
           </NavLink>
-          <NavLink to="/contact" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
+          <NavLink to="/contact" className={`${({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")} ${location.pathname === "/" && "text-white"}`}>
             Contact Us
           </NavLink>
         </div>
